59152705b951e497da45d054b10a27aeef3898e4
Elena Rybakova <lelkya84@gmail.com>
2024-09-04 13:05:59 +0300

    done

--- src/components/EndGameModal/EndGameModal.jsx
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -4,23 +4,51 @@ import { Button } from "../Button/Button";
 
 import deadImageUrl from "./images/dead.png";
 import celebrationImageUrl from "./images/celebration.png";
-// import { useContext, useState } from "react";
-// import { LeaderBoardContext } from "../../context";
+import { useContext, useState } from "react";
+import { LeaderBoardContext } from "../../context";
 import { Link } from "react-router-dom";
+import { addLeader } from "../../api";
 
 export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick, mode }) {
   const title = isWon ? "Вы победили!" : "Вы проиграли!";
   const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
   const imgAlt = isWon ? "celebration emodji" : "dead emodji";
-  const isLeader = true;
-  // const { leaderBoard, setLeaderBoard } = useContext(LeaderBoardContext);
-  // const { name, setName } = useState("Пользователь");
+  let isLeader = false;
+  const time = gameDurationMinutes * 60 + gameDurationSeconds;
+  const { leaderBoard, setLeaderBoard } = useContext(LeaderBoardContext);
+  const [name, setName] = useState("Пользователь");
+
+  if (isWon && mode.amount === 9 && (leaderBoard < 10 || leaderBoard[9].time < time)) {
+    isLeader = true;
+  }
+
+  async function onButtonLeader() {
+    try {
+      const result = await addLeader({ name: name, time: time });
+      if ("error" in result) {
+        throw new Error(result.error);
+      }
+      setLeaderBoard(result.leaders);
+    } catch (e) {
+      alert("Ошибка добавления. Попробуйте еще раз позже.");
+    }
+  }
 
   return (
     <div className={styles.modal}>
       <img className={styles.image} src={imgSrc} alt={imgAlt} />
       <h2 className={styles.title}>{isLeader ? "Вы попали на Лидерборд!" : title}</h2>
-      {isLeader && <input className={styles.input} placeholder="Пользователь" name="name" />}
+      {isLeader && (
+        <input
+          className={styles.input}
+          placeholder="Пользователь"
+          name="name"
+          label="Пользователь"
+          // eslint-disable-next-line no-restricted-globals
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      )}
       <p className={styles.description}>Затраченное время:</p>
       <div className={styles.time}>
         {gameDurationMinutes.toString().padStart("2", "0")}.{gameDurationSeconds.toString().padStart("2", "0")}
@@ -28,7 +56,12 @@ export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, 
 
       <Button onClick={onClick}>Начать сначала</Button>
       {isLeader && (
-        <button className={styles.leaderButton} onClick={() => {}}>
+        <button
+          className={styles.leaderButton}
+          onClick={() => {
+            onButtonLeader();
+          }}
+        >
           <Link to="/leaderboard" className={styles.leaderLink}>
             Добавить в лидерборд
           </Link>
