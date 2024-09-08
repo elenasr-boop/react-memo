0269b2a348d0c9ac2d5a713b872ba4bae96cfae3
Elena Rybakova <lelkya84@gmail.com>
2024-09-08 11:55:39 +0300

    tooltip for leaderboard

--- src/components/Leaders.jsx
+++ src/components/Leaders.jsx
@@ -29,16 +29,26 @@ export function Leader({ arr }) {
           <p className={styles.num}>#{id + 1}</p>
           <p className={styles.name}>{el.name}</p>
           <div className={styles.achievements}>
-            <img
-              src={achievementCheck(el.achievements, 1) ? "./hard_mode.png" : "./easy_mode.png"}
-              alt=""
-              className={styles.achievement1}
-            />
-            <img
-              src={achievementCheck(el.achievements, 2) ? "./wo_achievement.png" : "./with_achievement.png"}
-              alt=""
-              className={styles.achievement2}
-            />
+            <div className={styles.achiev1}>
+              <img
+                src={achievementCheck(el.achievements, 1) ? "./hard_mode.png" : "./easy_mode.png"}
+                alt=""
+                className={styles.achievement1}
+              />
+              <div className={styles.tooltip1}>
+                Игра {achievementCheck(el.achievements, 1) ? "" : "не"} пройдена в сложном режиме
+              </div>
+            </div>
+            <div className={styles.achiev2}>
+              <img
+                src={achievementCheck(el.achievements, 2) ? "./wo_achievement.png" : "./with_achievement.png"}
+                alt=""
+                className={styles.achievement2}
+              />
+              <div className={styles.tooltip2}>
+                Игра пройдена {achievementCheck(el.achievements, 2) ? "без супер-сил" : "с супер-силами"}
+              </div>
+            </div>
           </div>
           <p className={styles.time}>{changeTime({ time: el.time })}</p>
         </div>
